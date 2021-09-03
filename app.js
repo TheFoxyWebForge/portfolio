338c7ac9108b18cab2889fcab6f7e29495afdb1b
sneha khandelwal <snehakhandelwal333@gmail.com>
2021-09-04 02:04:28 +0530

    make it better

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.get("/", function(req,res) {
 });
 
 
-app.listen("3000" , function() {
-  console.log("server started on port 3000");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running in port 3000");
 });
